ba61e7909a72f4de4fa2abdce7a4ee4889444eb4
romety2 <romety2@wp.pl>
2017-04-07 23:44:38 +0200

    dodano menu

--- docs/scripts/s1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import codecs
-with codecs.open('NHS_Postcode_Directory_Latest_Centroids.csv','r',encoding='windows-1250') as f:
-    text = f.read()
-with codecs.open('map.csv','w',encoding='windows-1250') as f:
-    f.write(text[3:])
